a416aae8e56839330ae237841a1b17405c092884
Aadil Khan <aadilkhan99a@gmail.com>
2022-01-25 20:38:33 +0530

    ATM.cpp

---
+++ ATM.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+float withdraw(int amt, float bal)
+{
+    bal -= (amt + 0.50);
+    return bal;
+}
+
+int main()
+{
+    int amount;
+    float balance;
+    cin >> amount;
+    cin >> balance;
+
+    if(amount%5!=0 || amount+0.50>balance)
+    {
+     cout<<balance<<endl;
+    }
+     else
+     {
+    cout << withdraw(amount, balance) << endl;    
+     }
+    
+    return 0;
+}
